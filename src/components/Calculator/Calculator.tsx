7f5564c4f123692dd51c3c164f13e358a080f441
Luka <lukakobaidze2006@gmail.com>
2023-03-20 19:03:08 +0400

    added keyboard event

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,70 +1,36 @@
-import { useState } from 'react';
-import keypadKeys from './keypad-keys';
-import calculate from './calculator-calculate';
+import { useEffect, useState } from 'react';
+import { calculate, getType, transformValue } from './helpers';
 import Screen from './Screen';
 import Keypad from './Keypad';
-
-const typeofKey = (
-  value: string | undefined
-): 'number' | 'operator' | 'decimal' | 'other' | undefined => {
-  if (!value) return undefined;
-  if (Object.values(keypadKeys.operators).indexOf(value.trim()) !== -1) {
-    return 'operator';
-  } else if (value.trim() === '.') {
-    return 'decimal';
-  } else if (typeof +value === 'number' && !isNaN(+value)) {
-    return 'number';
-  } else {
-    return 'other';
-  }
-};
-
-const transformValue = (value: string | number, enteredValue: string) => {
-  const stringValue = String(value);
-  const lastValue = enteredValue.slice(-1);
-  if (
-    typeofKey(stringValue) === 'operator' ||
-    ((typeofKey(stringValue) === 'number' ||
-      typeofKey(stringValue) === 'decimal') &&
-      typeofKey(lastValue) === 'operator')
-  ) {
-    return ' ' + stringValue;
-  }
-
-  return stringValue;
-};
 
 const Calculator = () => {
   const [enteredValue, setEnteredValue] = useState('');
 
   const enterValueHandler = (value: string | number | undefined) => {
     if (!value && value !== 0) return;
-    setEnteredValue(prevValue => {
+    setEnteredValue((prevValue) => {
       const transformedValue = transformValue(value, prevValue);
       const lastValue = prevValue.split(' ')[prevValue.split(' ').length - 1];
-      if (typeofKey(transformedValue) === 'number') {
+      if (getType(transformedValue) === 'number') {
         if (lastValue === '0') {
           const lastValueRemoved = prevValue.split(' ').slice(0, -1);
           return lastValueRemoved.join(' ') + transformedValue;
         }
         return (prevValue += transformedValue);
       }
-      if (typeofKey(transformedValue) === 'operator') {
+      if (getType(transformedValue) === 'operator') {
         if (!prevValue && transformedValue.trim() !== '-') {
           return prevValue;
         }
 
-        if (typeofKey(lastValue) === 'operator') {
+        if (getType(lastValue) === 'operator') {
           const lastValueRemoved = prevValue.split(' ').slice(0, -1).join(' ');
           return lastValueRemoved + transformedValue;
         } else {
           return (prevValue += transformedValue);
         }
       }
-      if (
-        typeofKey(transformedValue) === 'decimal' &&
-        lastValue.indexOf('.') === -1
-      ) {
+      if (getType(transformedValue) === 'decimal' && lastValue.indexOf('.') === -1) {
         return (prevValue += transformedValue);
       }
       return prevValue;
@@ -72,7 +38,7 @@ const Calculator = () => {
   };
 
   const deleteHandler = () => {
-    setEnteredValue(prevValue => prevValue.slice(0, -1));
+    setEnteredValue((prevValue) => prevValue.slice(0, -1).trim());
   };
 
   const resetHandler = () => {
@@ -80,7 +46,7 @@ const Calculator = () => {
   };
 
   const equalHandler = () => {
-    setEnteredValue(prevValue => {
+    setEnteredValue((prevValue) => {
       if (!prevValue || prevValue.split(' ').length < 3) {
         return prevValue;
       }
@@ -88,6 +54,59 @@ const Calculator = () => {
       return calculate(prevValue.split(' '));
     });
   };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      console.log(e.key);
+      switch (e.key) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+          enterValueHandler(e.key);
+          break;
+        case '.':
+        case '>':
+          enterValueHandler('.');
+          break;
+        case '-':
+        case '_':
+          enterValueHandler('-');
+          break;
+        case '+':
+        case '=':
+          enterValueHandler('+');
+          break;
+        case '/':
+        case '?':
+          enterValueHandler('/');
+          break;
+        case '*':
+        case 'x':
+          enterValueHandler('x');
+          break;
+        case 'Backspace':
+        case 'Delete':
+          deleteHandler();
+          break;
+        case 'Enter':
+          equalHandler();
+          break;
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
